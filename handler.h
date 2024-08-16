7016efe6a5e71f5b17ed995ca2902e59db73e6a7
ssoonan <ssoonan0770@gmail.com>
2024-08-16 17:03:55 +0900

    ✨ uart, EL0

--- handler.h
+++ handler.h
@@ -1,12 +1,7 @@
-
 #ifndef _HANDLER_H
 #define _HANDLER_H
-#include "stdint.h"
 
-void enable_timer(void);
-uint32_t check_timer_status(void);
-void set_timer_interval(uint32_t value);
-uint32_t read_timer_freq(void);
+void init_interrupt_controller(void);
 void init_timer(void);
 void enable_irq(void);
 
--- handler.s
+++ handler.s
@@ -2,8 +2,8 @@
 .global vector_table
 .global enable_timer
 .global read_timer_freq
+.global read_timer_status
 .global set_timer_interval
-.global check_timer_status
 .global enable_irq
 
 .balign 0x800
@@ -41,11 +41,11 @@ current_el_spn_serror:
 
 .balign 0x80
 lower_el_aarch64_sync:
-    b error
+    b sync_handler
 
 .balign 0x80
 lower_el_aarch64_irq:
-    b error
+    b irq_handler
 
 .balign 0x80
 lower_el_aarch64_fiq:
@@ -73,132 +73,161 @@ lower_el_aarch32_serror:
 
 
 sync_handler:
-    sub sp, sp, #256
-    STP x0, x1, [sp]        
-    STP x2, x3, [sp, #16]   
-    STP x4, x5, [sp, #32]   
-    STP x6, x7, [sp, #48]   
-    STP x8, x9, [sp, #64]   
-    STP x10, x11, [sp, #80] 
-    STP x12, x13, [sp, #96] 
-    STP x14, x15, [sp, #112]
-    STP x16, x17, [sp, #128]
-    STP x18, x19, [sp, #144]
-    STP x20, x21, [sp, #160]
-    STP x22, x23, [sp, #176]
-    STP x24, x25, [sp, #192]
-    STP x26, x27, [sp, #208]
-    STP x28, x29, [sp, #224]
-    STP x30, xzr, [sp, #240]
-
+    sub sp, sp, #(32 * 8)
+    stp x0, x1, [sp]
+    stp x2, x3, [sp, #(16 * 1)]
+    stp x4, x5, [sp, #(16 * 2)]
+    stp	x6, x7, [sp, #(16 * 3)]
+	stp	x8, x9, [sp, #(16 * 4)]
+	stp	x10, x11, [sp, #(16 * 5)]
+	stp	x12, x13, [sp, #(16 * 6)]
+	stp	x14, x15, [sp, #(16 * 7)]
+	stp	x16, x17, [sp, #(16 * 8)]
+	stp	x18, x19, [sp, #(16 * 9)]
+	stp	x20, x21, [sp, #(16 * 10)]
+	stp	x22, x23, [sp, #(16 * 11)]
+	stp	x24, x25, [sp, #(16 * 12)]
+	stp	x26, x27, [sp, #(16 * 13)]
+	stp	x28, x29, [sp, #(16 * 14)]
+    str x30, [sp, #(16 * 15)]
 
     mov x0, #1
-    mrs x1, esr_el1
+    mrs x1, spsr_el1 //esr_el1
     mrs x2, elr_el1
     bl handler
 
-    LDP x0, x1, [sp]        
-    LDP x2, x3, [sp, #16]   
-    LDP x4, x5, [sp, #32]   
-    LDP x6, x7, [sp, #48]   
-    LDP x8, x9, [sp, #64]   
-    LDP x10, x11, [sp, #80] 
-    LDP x12, x13, [sp, #96] 
-    LDP x14, x15, [sp, #112]
-    LDP x16, x17, [sp, #128]
-    LDP x18, x19, [sp, #144]
-    LDP x20, x21, [sp, #160]
-    LDP x22, x23, [sp, #176]
-    LDP x24, x25, [sp, #192]
-    LDP x26, x27, [sp, #208]
-    LDP x28, x29, [sp, #224]
-    LDP x30, xzr, [sp, #240]
+    ldp x0, x1, [sp]
+    ldp x2, x3, [sp, #(16 * 1)]
+    ldp x4, x5, [sp, #(16 * 2)]
+    ldp	x6, x7, [sp, #(16 * 3)]
+	ldp	x8, x9, [sp, #(16 * 4)]
+	ldp	x10, x11, [sp, #(16 * 5)]
+	ldp	x12, x13, [sp, #(16 * 6)]
+	ldp	x14, x15, [sp, #(16 * 7)]
+	ldp	x16, x17, [sp, #(16 * 8)]
+	ldp	x18, x19, [sp, #(16 * 9)]
+	ldp	x20, x21, [sp, #(16 * 10)]
+	ldp	x22, x23, [sp, #(16 * 11)]
+	ldp	x24, x25, [sp, #(16 * 12)]
+	ldp	x26, x27, [sp, #(16 * 13)]
+	ldp	x28, x29, [sp, #(16 * 14)]
+    ldr x30, [sp, #(16 * 15)]
 
-    add sp, sp, #256
+    add sp, sp, #(32 * 8)
     eret
 
 irq_handler:
-    sub sp, sp, #256
-    STP x0, x1, [sp]        
-    STP x2, x3, [sp, #16]   
-    STP x4, x5, [sp, #32]   
-    STP x6, x7, [sp, #48]   
-    STP x8, x9, [sp, #64]   
-    STP x10, x11, [sp, #80] 
-    STP x12, x13, [sp, #96] 
-    STP x14, x15, [sp, #112]
-    STP x16, x17, [sp, #128]
-    STP x18, x19, [sp, #144]
-    STP x20, x21, [sp, #160]
-    STP x22, x23, [sp, #176]
-    STP x24, x25, [sp, #192]
-    STP x26, x27, [sp, #208]
-    STP x28, x29, [sp, #224]
-    STP x30, xzr, [sp, #240]
-
+    sub sp, sp, #(32 * 8)
+    stp x0, x1, [sp]
+    stp x2, x3, [sp, #(16 * 1)]
+    stp x4, x5, [sp, #(16 * 2)]
+    stp	x6, x7, [sp, #(16 * 3)]
+	stp	x8, x9, [sp, #(16 * 4)]
+	stp	x10, x11, [sp, #(16 * 5)]
+	stp	x12, x13, [sp, #(16 * 6)]
+	stp	x14, x15, [sp, #(16 * 7)]
+	stp	x16, x17, [sp, #(16 * 8)]
+	stp	x18, x19, [sp, #(16 * 9)]
+	stp	x20, x21, [sp, #(16 * 10)]
+	stp	x22, x23, [sp, #(16 * 11)]
+	stp	x24, x25, [sp, #(16 * 12)]
+	stp	x26, x27, [sp, #(16 * 13)]
+	stp	x28, x29, [sp, #(16 * 14)]
+    str x30, [sp, #(16 * 15)]
 
     mov x0, #2
-    mrs x1, esr_el1
+    mrs x1, spsr_el1 //esr_el1
     mrs x2, elr_el1
     bl handler
 
-    LDP x0, x1, [sp]        
-    LDP x2, x3, [sp, #16]   
-    LDP x4, x5, [sp, #32]   
-    LDP x6, x7, [sp, #48]   
-    LDP x8, x9, [sp, #64]   
-    LDP x10, x11, [sp, #80] 
-    LDP x12, x13, [sp, #96] 
-    LDP x14, x15, [sp, #112]
-    LDP x16, x17, [sp, #128]
-    LDP x18, x19, [sp, #144]
-    LDP x20, x21, [sp, #160]
-    LDP x22, x23, [sp, #176]
-    LDP x24, x25, [sp, #192]
-    LDP x26, x27, [sp, #208]
-    LDP x28, x29, [sp, #224]
-    LDP x30, xzr, [sp, #240]
+    ldp x0, x1, [sp]
+    ldp x2, x3, [sp, #(16 * 1)]
+    ldp x4, x5, [sp, #(16 * 2)]
+    ldp	x6, x7, [sp, #(16 * 3)]
+	ldp	x8, x9, [sp, #(16 * 4)]
+	ldp	x10, x11, [sp, #(16 * 5)]
+	ldp	x12, x13, [sp, #(16 * 6)]
+	ldp	x14, x15, [sp, #(16 * 7)]
+	ldp	x16, x17, [sp, #(16 * 8)]
+	ldp	x18, x19, [sp, #(16 * 9)]
+	ldp	x20, x21, [sp, #(16 * 10)]
+	ldp	x22, x23, [sp, #(16 * 11)]
+	ldp	x24, x25, [sp, #(16 * 12)]
+	ldp	x26, x27, [sp, #(16 * 13)]
+	ldp	x28, x29, [sp, #(16 * 14)]
+    ldr x30, [sp, #(16 * 15)]
 
-    add sp, sp, #256
+    add sp, sp, #(32 * 8)
     eret
 
 error:
+    sub sp, sp, #(32 * 8)
+    stp x0, x1, [sp]
+    stp x2, x3, [sp, #(16 * 1)]
+    stp x4, x5, [sp, #(16 * 2)]
+    stp	x6, x7, [sp, #(16 * 3)]
+	stp	x8, x9, [sp, #(16 * 4)]
+	stp	x10, x11, [sp, #(16 * 5)]
+	stp	x12, x13, [sp, #(16 * 6)]
+	stp	x14, x15, [sp, #(16 * 7)]
+	stp	x16, x17, [sp, #(16 * 8)]
+	stp	x18, x19, [sp, #(16 * 9)]
+	stp	x20, x21, [sp, #(16 * 10)]
+	stp	x22, x23, [sp, #(16 * 11)]
+	stp	x24, x25, [sp, #(16 * 12)]
+	stp	x26, x27, [sp, #(16 * 13)]
+	stp	x28, x29, [sp, #(16 * 14)]
+    str x30, [sp, #(16 * 15)]
+
     mov x0, #0
     bl handler
 
+    ldp x0, x1, [sp]
+    ldp x2, x3, [sp, #(16 * 1)]
+    ldp x4, x5, [sp, #(16 * 2)]
+    ldp	x6, x7, [sp, #(16 * 3)]
+	ldp	x8, x9, [sp, #(16 * 4)]
+	ldp	x10, x11, [sp, #(16 * 5)]
+	ldp	x12, x13, [sp, #(16 * 6)]
+	ldp	x14, x15, [sp, #(16 * 7)]
+	ldp	x16, x17, [sp, #(16 * 8)]
+	ldp	x18, x19, [sp, #(16 * 9)]
+	ldp	x20, x21, [sp, #(16 * 10)]
+	ldp	x22, x23, [sp, #(16 * 11)]
+	ldp	x24, x25, [sp, #(16 * 12)]
+	ldp	x26, x27, [sp, #(16 * 13)]
+	ldp	x28, x29, [sp, #(16 * 14)]
+    ldr x30, [sp, #(16 * 15)]
+
+    add sp, sp, #(32 * 8)
     eret
     
-
 read_timer_freq:
     mrs x0, CNTFRQ_EL0
     ret
 
 set_timer_interval:
-    msr CNTP_TVAL_EL0, x0
-    ret
-
-enable_timer_register:
-    mov x0, #1
-    msr CNTP_CTL_EL0, x0
+    msr CNTP_TVAL_El0, x0
     ret
 
 enable_timer:
     stp x29, x30, [sp, #-16]!
-    
+
     bl read_timer_freq
     mov x1, #100
     udiv x0, x0, x1
     bl set_timer_interval
-    bl enable_timer_register
+
+    mov x0, #1
+    msr CNTP_CTL_EL0, x0
 
     ldp x29, x30, [sp], #16
     ret
 
-check_timer_status:
+read_timer_status:
     mrs x0, CNTP_CTL_EL0
     ret
 
 enable_irq:
-    mov x0, #(0 << 7)
-    msr daif, x0
+    msr daifclr, #2
     ret
